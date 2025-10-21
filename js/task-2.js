c3f4ca2e91ec266f8eb9413cef49fcc72d501e5f
GennadiyS <geeeshaaa@gmail.com>
2025-10-21 21:18:55 +0300

    HW4_v2

--- js/task-2.js
+++ js/task-2.js
@@ -1,35 +1,35 @@
-function calcAverageCalories(days){
- let avg = 0;
-  for(const day in days){
-avg += days[day].calories;
-}
-return avg > 0 ? avg /= days.length: [];
+function calcAverageCalories(days) {
+  let sum = 0;
+  for (const day of days) {
+    sum += day.calories;
+  }
+  let result = 0;
+  if (sum > 0) result = sum / days.length;
+  return result;
 }
 
 console.log(
   calcAverageCalories([
-    { day: "monday", calories: 3010 },
-    { day: "tuesday", calories: 3200 },
-    { day: "wednesday", calories: 3120 },
-    { day: "thursday", calories: 2900 },
-    { day: "friday", calories: 3450 },
-    { day: "saturday", calories: 3280 },
-    { day: "sunday", calories: 3300 }
+    { day: 'monday', calories: 3010 },
+    { day: 'tuesday', calories: 3200 },
+    { day: 'wednesday', calories: 3120 },
+    { day: 'thursday', calories: 2900 },
+    { day: 'friday', calories: 3450 },
+    { day: 'saturday', calories: 3280 },
+    { day: 'sunday', calories: 3300 },
   ])
 ); // 3180
 
 console.log(
   calcAverageCalories([
-    { day: "monday", calories: 2040 },
-    { day: "tuesday", calories: 2270 },
-    { day: "wednesday", calories: 2420 },
-    { day: "thursday", calories: 1900 },
-    { day: "friday", calories: 2370 },
-    { day: "saturday", calories: 2280 },
-    { day: "sunday", calories: 2610 }
+    { day: 'monday', calories: 2040 },
+    { day: 'tuesday', calories: 2270 },
+    { day: 'wednesday', calories: 2420 },
+    { day: 'thursday', calories: 1900 },
+    { day: 'friday', calories: 2370 },
+    { day: 'saturday', calories: 2280 },
+    { day: 'sunday', calories: 2610 },
   ])
 ); // 2270
 
-console.log(
-  calcAverageCalories([])
-); // 0
+console.log(calcAverageCalories([])); // 0
